a724ca24ef77df63722c8dc6c555c7dd31978f30
Kalambur4k <63010727+Kalambur4k@users.noreply.github.com>
2021-12-20 03:43:18 +0300

    Add files via upload

--- lab4/check_mm.c
+++ lab4/check_mm.c
@@ -6,10 +6,31 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
-#define MAXLEN 200
 #define SEMA_NAME "/semaphore_for_pipe1_1d5LFo4"
 #define SEMB_NAME "/semaphore_for_pipe2_1d5LFo4"
 #define SHARED_MEMORY_OBJECT_NAME "shared_memory_1d5LFo4"
+#define SHARED_MEMORY_SIZE 2
+
+sem_t *sema, *semb;
+int shm;
+char *addr;
+
+int sema_ok = 0, semb_ok = 0, shm_ok = 0, mmap_ok = 0, file_ok = 0;
+
+void clean_all() {
+    if ( mmap_ok ) {
+        munmap( addr, SHARED_MEMORY_SIZE );        
+    }
+    if ( shm_ok ) {
+        close( shm );
+    }
+    if ( semb_ok ) {
+        sem_close(semb);
+    }
+    if ( sema_ok ) {
+        sem_close(sema);
+    }
+}
 
 int main()
 {
@@ -21,71 +42,64 @@ int main()
         perror("Cannot open semaphore A");
         return EXIT_FAILURE;
     }
+    sema_ok = 1;
     
     semb = sem_open(SEMB_NAME, 0);
     if ( semb == SEM_FAILED ) {
         perror("Cannot open semaphore B");
+        clean_all();
         return EXIT_FAILURE;
     }
-    
-    if (sem_wait(sema) < 0 ) {
-        perror("Cannot wait semaphore a");    
-        return EXIT_FAILURE;
-    }
+    semb_ok = 1;
     
     int shm = shm_open(SHARED_MEMORY_OBJECT_NAME, O_RDWR, 0777);
     if ( shm < 0 ) {
         perror("Cannot open shared memory object");
+        clean_all();
         return EXIT_FAILURE;
     }
+    shm_ok = 1;
     
-    char *addr = mmap(0, MAXLEN+1, PROT_WRITE|PROT_READ, MAP_SHARED, shm, 0);
+    char *addr = mmap(0, SHARED_MEMORY_SIZE, PROT_WRITE|PROT_READ, MAP_SHARED, shm, 0);
     if ( addr == (char*)-1 ) {
         perror("Cannot do mmap");
+        clean_all();
+        return EXIT_FAILURE;
+    }
+    mmap_ok = 1;
+    
+    char *test = addr + 1;
+    
+    if ( sem_wait(sema) < 0 ) {
+        perror("Cannot wait semaphore a");
+        clean_all();
         return EXIT_FAILURE;
     }
     
-    FILE* fp = fopen(addr, "w");
-    if (!fp) {
-        perror("File create failed");
-        return EXIT_FAILURE;
-    }
-    
-    if ( sem_post( semb ) < 0 ) {
-        perror("Cannot post semaphore b");    
-        return EXIT_FAILURE;
-    }
+    while ( *test ) {
         
-    if (sem_wait(sema) < 0 ) {
-        perror("Cannot wait semaphore a");    
-        return EXIT_FAILURE;
-    }
-    
-    while ( *addr ) {
-        
-        if ( isupper( *addr ) ) {
-            fprintf( fp, "%s\n", addr );
+        //printf( "Child check: test is %c\n", *test );
+        if ( isupper( *test ) ) {
             *addr = 0;
         } else {
-            sprintf( addr, "Error: %s", addr );            
+            *addr = 1;
         }
+        //printf( "Child check result: %d\n", (int)*addr );
 
         if ( sem_post( semb ) < 0 ) {
-            perror("Cannot post semaphore b");    
+            perror("Cannot post semaphore b");
+            clean_all();
             return EXIT_FAILURE;
         }
             
         if (sem_wait(sema) < 0 ) {
-            perror("Cannot wait semaphore a");    
+            perror("Cannot wait semaphore a");
+            clean_all();
             return EXIT_FAILURE;
         }
         
     }
     
-    fclose( fp );
-    
-    munmap( addr, MAXLEN );
-    close( shm );
-
+    clean_all();
 }    
     
--- lab4/master_mm.c
+++ lab4/master_mm.c
@@ -1,81 +1,121 @@
-#include <pthread.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
+
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 #define MAXLEN 200
+#define SEMA_NAME "/semaphore_for_pipe1_1d5LFo4"
+#define SEMB_NAME "/semaphore_for_pipe2_1d5LFo4"
+#define SHARED_MEMORY_OBJECT_NAME "shared_memory_1d5LFo4"
+#define SHARED_MEMORY_SIZE 2
 
+sem_t *sema, *semb;
+int shm;
+char *addr;
+FILE* fp;
 
-void *read_check_errors(void *arg) {
-    int fd = *(int*)arg;
-    char buf[MAXLEN];
-    int readen = read(fd,buf, MAXLEN);
-    while (readen > 0) {
-        write(STDOUT_FILENO,buf,readen);
-        readen = read(fd,buf, MAXLEN);
+int sema_ok = 0, semb_ok = 0, shm_ok = 0, mmap_ok = 0, file_ok = 0;
+
+void clean_all() {
+    if ( file_ok ) {
+        fclose( fp );
     }
-    close(fd);
-    pthread_exit(NULL);
+    if ( mmap_ok ) {
+        munmap( addr, SHARED_MEMORY_SIZE );        
+    }
+    if ( shm_ok ) {
+        close( shm );
+        shm_unlink(SHARED_MEMORY_OBJECT_NAME);
+    }
+    if ( semb_ok ) {
+        sem_close(semb);
+        sem_unlink(SEMB_NAME);        
+    }
+    if ( sema_ok ) {
+        sem_close(sema);
+        sem_unlink(SEMA_NAME);        
+    }
 }
 
 int main(void)
 {
-    int fda[2],fdb[2]; 
-    if ( pipe(fda) < 0 || pipe(fdb) < 0 ) {
-        perror("Cannot create pipe");
+    
+    sema = sem_open(SEMA_NAME, O_CREAT, 0777, 0);
+    if ( sema == SEM_FAILED ) {
+        perror("Cannot create semaphore A");
         return EXIT_FAILURE;
     }
+    sema_ok = 1;
+    
+    semb = sem_open(SEMB_NAME, O_CREAT, 0777, 0);
+    if ( semb == SEM_FAILED ) {
+        perror("Cannot create semaphore B");
+        clean_all();
+        return EXIT_FAILURE;
+    }
+    semb_ok = 1;
+
+    shm = shm_open(SHARED_MEMORY_OBJECT_NAME, O_CREAT|O_RDWR, 0777);
+    if ( shm < 0 ) {
+        perror("Cannot create shared memory object");
+        clean_all();
+        return EXIT_FAILURE;
+    }
+    shm_ok = 1;
+    
+    if ( ftruncate(shm, SHARED_MEMORY_SIZE) == -1 ) {
+        perror("Cannot set shared memory object size");
+        clean_all();
+        return EXIT_FAILURE;
+    }    
+    
+    addr = mmap(0, SHARED_MEMORY_SIZE, PROT_WRITE|PROT_READ, MAP_SHARED, shm, 0);
+    if ( addr == (char*)-1 ) {
+        perror("Cannot do mmap");
+        clean_all();
+        return EXIT_FAILURE;
+    }    
+    mmap_ok = 1;
+        
     char fname[MAXLEN];
     //input filename
     printf("Input file name: ");
     scanf("%[^\n]s", fname);
-    getc(stdin); 
-    //open file
-    FILE* fp = fopen(fname, "w");
+    getc(stdin); // убрать перевод строки
+    fp = fopen(fname, "w");
     if (!fp) {
         perror("Cannot create file");
+        clean_all();
         return EXIT_FAILURE;
     }
+    file_ok = 1;
+    
     // FORK
     int id = fork();
     //error FORK
     if (id == -1)
     {
         perror("Fork error");
+        clean_all();
         return EXIT_FAILURE;
     }
     //CHILD WORK
     else if (id == 0)
     {
-        close(STDIN_FILENO);
-        dup(fda[0]);
-        close(fda[0]);
-        
-        close(STDOUT_FILENO);
-        dup(fileno(fp));
-        close(fileno(fp));
-        
-        close(STDERR_FILENO);
-        dup(fdb[1]);
-        close(fdb[1]);
-        
-        close(fda[1]);
-        close(fdb[0]);
-        execlp("./check", NULL);
+        execlp("./check_mm", NULL);
     }
     //PARENT WORK
     else
     {
-        close(fda[0]);
-        close(fdb[1]);
-        close(fileno(fp));
-        
-        pthread_t err_thread;
-        if ( pthread_create( &(err_thread), NULL, read_check_errors, (void*) &(fdb[0]) ) != 0 ) {
-            perror("Cannot create thread for errors");
-            return EXIT_FAILURE;
-        }
+        // в первом байте разделяемой памяти будем хранить результат проверки: 0 = ok, 1 = не ok
+        // во втором байте разместим первую букву строки, которую нам и нужно будет проверить
+        char *test = addr + 1;
+        //printf( "Shared memory is addr = %p, test = %p\n", addr, test );
         
         char buffer[MAXLEN];
         char *buf = buffer;
@@ -83,13 +123,40 @@ int main(void)
         
         int len = getline(&buf,&buf_size,stdin); 
         while ( len != EOF ) {
-            write( fda[1], buffer, len );
+
+            //printf( "Master: %zu len %s", len, buf );
+            *test = buffer[0];
+
+            if ( sem_post( sema ) < 0 ) {
+                perror("Cannot post semaphore a");    
+                clean_all();
+                return EXIT_FAILURE;
+            }
+            
+            if (sem_wait(semb) < 0 ) {
+                perror("Cannot wait semaphore b");    
+                clean_all();
+                return EXIT_FAILURE;
+            }
+            
+            if ( *addr ) {
+                printf( "Error: %s", buf );
+            } else {
+                fprintf( fp, buf );
+            }
             len = getline(&buf,&buf_size,stdin); 
         }
-        close(fda[1]);
         
-        pthread_join( err_thread, NULL );        
+        *test = 0;
+        if ( sem_post( sema ) < 0 ) {
+            perror("Cannot post semaphore a");    
+            clean_all();
+            return EXIT_FAILURE;
+        }
+
         wait(NULL);
+        clean_all();
+        
     }
     return 0;
 }
